b68b3b5eeff8d68c4eced3988e688b1845ad9ffe
Kush Agarwal <agrawalkush783@gmail.com>
2024-11-23 22:59:24 +0530

    tested the create user endpoint

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,7 +1,33 @@
 const { prismaClient } = require("../config/database");
 
 const contactChecker = async (req, res) => {
-    
+    const { email, phoneNumber } = req.body;
+
+    if (!email && !phoneNumber) {
+        return res.status(400).json({
+            message: "At least one of email or phoneNumber must be provided.",
+        });
+    }
+
+    const contacts = await prismaClient.contact.findMany({
+        where: {
+            OR: [{ email: email }, { phoneNumber: phoneNumber }],
+        },
+        orderBy: { createdAt: "asc" },
+    });
+
+    if (contacts.length === 0) {
+        const newContact = await prismaClient.contact.create({
+            data: {
+                email,
+                phoneNumber,
+                linkPrecedence: "primary",
+            },
+        });
+        return res.status(200).json({ message: newContact });
+    }
+
+    return res.status(200).json({ message: "User Already exists", contacts });
 };
 
 module.exports = { contactChecker };
--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const port = 4000;
 dotenv.config();
 connectDB();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.send("Welcome to the Prisma API!");
 });
